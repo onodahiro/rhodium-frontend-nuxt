c78b9394159774beb13dc4b38fbb6ee0b09f2c49
Maksim Zakharov <m.zakharov@tg.dunice.net>
2024-09-10 17:27:19 +0300

    add vuetify, sass, upd config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,28 @@
-import { defineNuxtConfig } from 'nuxt/config'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', (_options, nuxt) => {
+    nuxt.hooks.hook('vite:extendConfig', (config) => {
+      // @ts-expect-error disable config error
+      config.plugins.push(vuetify({ autoImport: true }))
+    })
+  }],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  css: [
+    'vuetify/lib/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.css',
+  ],
+  build: {
+    transpile: ['vuetify'],
+  },
   components: [
     {
       path: '~/components',
@@ -17,6 +36,8 @@ export default defineNuxtConfig({
     },
   },
   runtimeConfig: {
-    apiNotes: '',
+    public: {
+      apiNotes: '',
+    },
   },
 })
